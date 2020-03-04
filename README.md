f8f33fc32e1e4ec50bd5a7d8d2eed5b5687c1225
Uniqsy <m15265723639@163.com>
2020-03-04 23:33:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+there is my beego qq bot
+for the linux cloud server
+
---
+++ controllers/default.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/astaxie/beego"
+	"encoding/json"
+	"net/http"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+type QQController struct {
+	beego.Controller
+}
+
+type MessageQQ struct {
+	post_type 		string
+	usr_id 			string
+	message 		string
+}
+
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+
+func reply_message(user MessageQQ) {
+	url := "http://192.168.0.1:5700/send_private_msg"
+	jsonReply,_ := json.Marshal(user)
+	http.NewRequest("POST", url, bytes.NewBuffer(jsonReply))
+	fmt.Println("ok")
+}
+
+func (this *QQController) Post() {
+	var user MessageQQ
+	data := this.Ctx.Input.RequestBody
+	err := json.Unmarshal(data, &user)
+	if err != nil {
+		fmt.Println("json.Unmarshal is err:", err.Error())
+	}
+	if user.post_type == "message" {
+		reply_message(user)
+	}
+}
