488f668d9150146ff70019687d50e9a4f1e6bc84
Uniqsy <m15265723639@163.com>
2020-03-05 22:10:03 +0800

    加好友“

--- controllers/default.go
+++ controllers/default.go
@@ -14,7 +14,14 @@ type MainController struct {
 
 
 type MessageQQ struct {
+	Post_type		string`json:"post_type"`
+
+	Request_type	string`json:"request_type"`
+	Comment 		string`json:"comment"`
+	Flag  			string`json:"flag"`
+
 	Message_type	string`json:"message_type"`
+	Message_id		int64`json:"message_id"`
 
 	User_id 		int64`json:"user_id"`
 	Group_id		int64`json:"group_id"`
@@ -23,6 +30,10 @@ type MessageQQ struct {
 	Message 		string`json:"message"`
 }
 
+type AddFriendReply struct {
+	Flag			string`json:"flag"`
+	Approve			bool`json:"approve"`
+}
 
 func (c *MainController) Get() {
 	c.Ctx.WriteString("hello world")
@@ -34,7 +45,23 @@ func reply_message(user MessageQQ) {
 	json.NewEncoder(jsonReply).Encode(user)
 	reps, err := http.Post(urlSendMessage, "application/json;charset=utf-8", jsonReply)
 	if err != nil {
-		fmt.Println("there is some err about reply",  err.Error())
+		fmt.Println("there is some err about add friend reply",  err.Error())
+	} else {
+		fmt.Println(reps)
+	}
+}
+
+func add_friend(user MessageQQ) {
+	urlAddFriend := "http://192.168.0.1:5700/set_friend_add_request"
+	contentType := "application/json;charset=utf-8"
+	addReply := AddFriendReply{}
+	addReply.Approve = true
+	addReply.Flag = user.Flag
+	jsonReply := new(bytes.Buffer)
+	json.NewEncoder(jsonReply).Encode(addReply)
+	reps, err := http.Post(urlAddFriend, contentType, jsonReply)
+	if err != nil {
+		fmt.Println("there is some err about message reply",  err.Error())
 	} else {
 		fmt.Println(reps)
 	}
@@ -47,5 +74,15 @@ func (this *MainController) Post() {
 	if err != nil {
 		fmt.Println("json.Unmarshal is err:", err.Error())
 	}
-	reply_message(user)
+	if user.Post_type == "message" {
+		if user.Message_type == "private" {
+			reply_message(user)
+		} else if user.Message_type == "group" {
+			reply_message(user)
+		}
+	} else if user.Post_type == "request" {
+		if user.Request_type == "friend" {
+			add_friend(user)
+		}
+	}
 }
